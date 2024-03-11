a0d75f7429963c35ce6ad394bdce17f97d31a79a
SanSaniya <158427968+SanSaniya@users.noreply.github.com>
2024-03-11 14:31:20 +0530

    index.js

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 var inputvalue = document.querySelector('#cityinput');
 var btn = document.querySelector('#add');
 var city = document.querySelector('#cityoutput');
-var description  = document.querySelector('#description');
-var temp  = document.querySelector('#temp');
-var wind  = document.querySelector('#wind');
-apik = "0bcce8c4726ae566823d3db84174c42f";
-function convertion(val){
-    return (val-273).toFixed(3);
+var description = document.querySelector('#description');
+var temperature = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+var apik = "0bcce8c4726ae566823d3db84174c42f";
+
+function convertion(val) {
+    return (val - 273).toFixed(3);
 }
-btn.addEventListener('click' , function()
-{
-    
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}' + inputvalue.value + '&appid=' +apik)
-    .then(res => res.json())
-    .then(data =>
-        {   
+
+btn.addEventListener('click', function () {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&appid=${apik}`)
+        .then(res => res.json())
+        .then(data => {
             var nameval = data.name;
             var description = data.weather[0].description;
             var temperature = data.main.temp;
             var windspeed = data.wind.speed;
 
-
-            city.innerHTML = `Weather of <span> ${nameval} <span>`;
-            temp.innerHTML = `Temperature: <span> ${ convertion(temprature)}°C <span>`;
-            description.innerHTML = `Sky Conditions: <span> ${description}<span>`;
-            wind.innerHTML = `Wind Speed: <span> ${windspeed} km/h <span>`;
+            city.innerHTML = `Weather of <span>${nameval}</span>`;
+            temp.innerHTML = `Temperature: <span>${convertion(temperature)}°C</span>`;
+            description.innerHTML = `Sky Conditions: <span>${description}</span>`;
+            wind.innerHTML = `Wind Speed: <span>${windspeed} km/h</span>`;
         })
-        .catch(err => alert('You entered Wrong city name.'));
+        .catch(err => alert('You entered the wrong city name.'));
 });
